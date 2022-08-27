326a9d83b165aee67e83286e9943b24de5f96e93
adityamahakali <adityamahakali@gmail.com>
2022-08-27 06:39:05 +0530

    final

--- k35.py
+++ k35.py
@@ -6,11 +6,15 @@ import random
 SIZE = WIDTH,HEIGHT = 800,800
 BLACK = (0,0,0)
 WHITE = (255,255,255)
-greed_surface = pygame.image.load("background.jpeg")
+greed_surface = pygame.image.load("assets/pics/background.jpeg")
 greed_surface = pygame.transform.scale(greed_surface, (WIDTH ,HEIGHT))
 FPS = 60
 
+pygame.mixer.init()
+pygame.mixer.stop()
 
+fly = pygame.mixer.Sound("assets/sound/fly.mp3")
+won = pygame.mixer.Sound("assets/sound/won.mp3")
 def initialize_grid(m,n):
     # here m,n Indicate the dimension of the m X n grid
     cell_width = WIDTH/n
@@ -43,21 +47,44 @@ def select_random_movement(cell_width):
 def check(p1,p2):
     return (p1 == p2)
 
-def play_k35_random(*args,**kwargs):
+def play_random(player_identifier):
     pygame.init()
     SCREEN = pygame.display.set_mode(SIZE)
     CLOCK = pygame.time.Clock()
     SCREEN.fill(BLACK)
-    run = True
+    sprites = []
+    np=2     
     p1= player(0,0,"red.png",80)
     p2= player(WIDTH - 80,HEIGHT - 80,"purple.png",80)
-    p3= player(WIDTH - 80 ,0,"yellow.png",80)
+    p1_not_won = 1 
+    p2_not_one = 1
+    sprites.append(p1)
+    sprites.append(p2)
     trail_p1 = []
+    trail_p1.append((p1.image_loc.centerx,p1.image_loc.centery))
     trail_p2 = []
-    trail_p3 = []
+    trail_p1.append((p2.image_loc.centerx,p2.image_loc.centery))
+    trail_p3=[]
+    trail_p4=[]
+    points = []
+    if(player_identifier == '3P' or player_identifier=='4P'):
+        np =3
+        p3= player(WIDTH - 80 ,0,"yellow.png",80)
+        sprites.append(p3)
+        p3_not_one = 1
+        trail_p3.append((p3.image_loc.centerx,p3.image_loc.centery))
+        
+    if(player_identifier == '4P'):
+        np = 4
+        p4 = player(0,HEIGHT-80, "marine.png",80)
+        p4_not_one=1
+        trail_p4.append((p4.image_loc.centerx,p4.image_loc.centery))
+    run = True
     red = (255,0,0)
     purple  = (128,0,128)
     yellow = (0,255,255)
+    marine = (50,50,255)
+    colors = [red,purple,yellow,marine]
     ellipse_size = (0,0,10,10)
     while run:
         dt = CLOCK.tick(10)
@@ -73,24 +100,75 @@ def play_k35_random(*args,**kwargs):
         
         x1,y1 = select_random_movement(80)
         x2,y2 = select_random_movement(80)
-        x3,y3 = select_random_movement(80)
-        p1.move(x1,y1,600,600,dt)
-        p2.move(x2,y2,600,600,dt)
-        p3.move(x3,y3,600,600,dt)
+        p1.move(x1,y1,600,600,dt,p1_not_won)
+        p2.move(x2,y2,600,600,dt,p2_not_one)
         SCREEN.blit(p1.image,p1.image_loc)
         SCREEN.blit(p2.image,p2.image_loc)
-        SCREEN.blit(p3.image,p3.image_loc)
         trail_p1.append((p1.image_loc.centerx,p1.image_loc.centery))
         trail_p2.append((p2.image_loc.centerx,p2.image_loc.centery))
-        trail_p3.append((p3.image_loc.centerx,p3.image_loc.centery))
         for trail in trail_p1:
-            pygame.draw.ellipse(greed_surface,red,(trail[0]-2,trail[1]-2,10,10))
+            pygame.draw.ellipse(greed_surface,red,(trail[0]-5,trail[1]-5,10,10))
         for trail in trail_p2:
-            pygame.draw.ellipse(greed_surface,purple,(trail[0]+2,trail[1]+2,10,10))  
-        for trail in trail_p3:
-            pygame.draw.ellipse(greed_surface,yellow,(trail[0]-2,trail[1]+2,10,10))  
+            pygame.draw.ellipse(greed_surface,purple,(trail[0]+5,trail[1]+5,10,10))  
+        
+        if(np==3 or np ==4):
+            x3,y3 = select_random_movement(80)
+            p3.move(x3,y3,600,600,dt,p3_not_one)
+            SCREEN.blit(p3.image,p3.image_loc)
+            trail_p3.append((p3.image_loc.centerx,p3.image_loc.centery))
+            for trail in trail_p3:
+                pygame.draw.ellipse(greed_surface,yellow,(trail[0]-5,trail[1]+5,10,10))  
+        
+        if(np==4):
+            x4,y4 = select_random_movement(80)
+            p4.move(x4,y4,600,600,dt,p4_not_one)
+            SCREEN.blit(p4.image,p4.image_loc)
+            trail_p4.append((p4.image_loc.centerx,p4.image_loc.centery))
+            for trail in trail_p4:
+                pygame.draw.ellipse(greed_surface,marine,(trail[0]+5,trail[1]-5,10,10))  
+        
+        
         pygame.display.update()
-        if(check(p1.image_loc.center,p2.image_loc.center) or  check(p1.image_loc.center,p3.image_loc.center) or check(p3.image_loc.center,p2.image_loc.center)):
-            time.sleep(2)
-            run = False
+            
+        if(check(p1.image_loc.center,p2.image_loc.center)):
+            p1_not_won = 0
+            p2_not_one = 0
+        if(np==3 or np ==4):
+            if(check(p1.image_loc.center,p3.image_loc.center)):
+                p1_not_won = 0
+                p3_not_one = 0
+            if(check(p2.image_loc.center,p3.image_loc.center)):
+                p3_not_one = 0
+                p2_not_one = 0
+        if(np==4):
+            if(check(p1.image_loc.center,p4.image_loc.center)):
+                p1_not_won = 0
+                p4_not_one = 0
+            if(check(p2.image_loc.center,p4.image_loc.center)):
+                p4_not_one = 0
+                p2_not_one = 0
+            if(check(p4.image_loc.center,p3.image_loc.center)):
+                p3_not_one = 0
+                p4_not_one = 0
+        if(np<3):
+            if(p1_not_won  + p2_not_one == 0):
+                time.sleep(2)
+                run = False
+        elif(np==3):
+            if(p1_not_won  + p2_not_one + p3_not_one  == 0):
+                time.sleep(2)
+                run = False
+        else: 
+            if(p1_not_won  + p2_not_one + p3_not_one + p4_not_one == 0):
+                time.sleep(2)
+                run = False
+          
+def play_k35_random(*args,**kwargs):
+    print(args)
+    if(args[1][0]== 'RANDOM'):
+        play_random(args[0][0]) 
+    elif(args[1][0]== 'BFS'):
+        play_random(args[0][0])    
+    else:
+        play_random(args[0][0])   
     return
--- player.py
+++ player.py
@@ -4,22 +4,27 @@ class player:
          self.rad = cell_size
          self.x = start_x + cell_size /2
          self.y = start_y + cell_size /2
-         self.image = pygame.image.load(image)
+         self.image = pygame.image.load("assets/pics/" + image)
          self.image = pygame.transform.scale(self.image, (60 ,60))
          self.image_loc = self.image.get_rect()
          self.image_loc.center = self.x,self.y 
          
-    
-    def move(self,x,y,x_lim,y_lim,dt):
+    def move(self,x,y,x_lim,y_lim,dt,has_not_won=1):
         
         if(x>0 and x+self.x <= x_lim ):
-            self.x += x #* dt
+            self.x += x * has_not_won #* dt
         elif(y>0 and y+self.y <= y_lim):
-            self.y += y #* dt
+            self.y += y * has_not_won #* dt
         elif(x<0 and self.x +x  >= 0):
-            self.x += x #* dt
+            self.x += x * has_not_won #* dt
         elif(y<0 and self.y +y >=0):
-            self.y += y #* dt
+            self.y += y * has_not_won #* dt
         self.image_loc.center = self.x,self.y
        
-         
+# background music credits     
+# The following music was used for this media project:
+# Music: Adventures In Adventureland by Kevin MacLeod
+# Free download: https://filmmusic.io/song/8287-adventures-in-adventureland
+# License (CC BY 4.0): https://filmmusic.io/standard-license
+# Artist website: https://incompetech.com
+
